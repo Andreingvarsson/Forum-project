4c887faf3b4ea4cddd61e391bcf42d43a5eff4f3
André <andreingvarsson@hotmail.com>
2021-01-11 10:57:43 +0100

    Forum view, state of forum, threads / forum

---
+++ src/views/Forum.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="forum" class="forum">
+    <div class="forum-title">{{ forum.name }}</div>
+    <CreateThread />
+    <ThreadListItem
+      v-for="thread in forum.threads"
+      :key="thread.thread_id"
+      :thread="thread"
+    />
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+import CreateThread from "../views/CreateThread";
+import ThreadListItem from "../components/ThreadListItem";
+
+@Component({
+  components: { CreateThread, ThreadListItem },
+})
+class Forum extends Vue {
+  get forum() {
+    return this.$store.state.forum;
+  }
+
+  async created() {
+    this.$store.dispatch("fetchForumById", this.$route.params.id);
+  }
+
+  navigate(id) {
+    this.$store.commit("setThread", this.thread);
+    this.$router.push({ path: `/forum/thread/${id}` });
+  }
+}
+
+export default Forum;
+</script>
+
+<style scoped>
+.forum-title {
+  font-size: 2em;
+}
+</style>
