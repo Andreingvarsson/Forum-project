54ae4024fa7ef363174a1ee1761bf8c5f8978f28
André <andreingvarsson@hotmail.com>
2021-01-11 10:49:10 +0100

    working register account and login. fetches and states, put into navbar.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="navbar">
-    <h1>GamingForum</h1>
-      <router-link to="/login">Login</router-link> 
+    <h1 @click="goHome">GamingForum</h1>
+    <router-link v-if="!user" to="/login">Login</router-link>
+    <div v-if="user" @click="logoutUser">Logout</div>
   </div>
 </template>
 
@@ -9,22 +10,40 @@
 import { Vue, Component } from "vue-property-decorator";
 
 @Component
- class NavBar extends Vue {
+class NavBar extends Vue {
+  goHome() {
+    this.$router.push({ path: "/" });
+  }
+  get user() {
+    return this.$store.state.loggedInUser;
+  }
 
+  async logoutUser() {
+    fetch("/auth/logout", {
+      method: "GET",
+    })
+      .then((response) => {
+        if (response.ok) {
+          this.$store.commit("setLoggedInUser", null);
+          if (this.$route.path === "/") return;
+          this.$router.push("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
-
 export default NavBar;
 </script>
 
 <style scoped>
-.navbar{
-   align-items: center;
-   background: black;
-   height: 4em; 
+.navbar {
+  align-items: center;
+  background: black;
+  height: 4em;
 }
-.h1{
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    color: honeydew;
+.h1 {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
 }
-
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,63 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    loggedInUser: null,
+    forum: null,
+    forums: null,
+    thread: null,
+    threads: null,
   },
   mutations: {
+    setForums(state, data) {
+      state.forums = data;
+    },
+    setForum(state, data) {
+      state.forum = data;
+    },
+    createNewThread(state, data) {
+      state.forum.threads.push(data);
+    },
+    setThreads(state, data) {
+      state.thread = data;
+    },
+    setThread(state, data) {
+      state.thread = data;
+    },
+    setLoggedInUser(state, data) {
+      state.loggedInUser = data;
+    },
   },
   actions: {
+    async fetchAllForums({ commit }) {
+      console.log("Inne i fetchAllForums");
+      const forumResult = await fetch("/api/v1/forums");
+      const forums = await forumResult.json();
+      console.log(forums, "Alla forums");
+      commit("setForums", forums);
+    },
+
+    async fetchForumById({ commit }, id) {
+      console.log("inne i BYID");
+      const forumResult = await fetch(`/api/v1/forums/${id}`);
+      const forum = await forumResult.json();
+      console.log(forum, "ett forum");
+      commit("setForum", forum);
+    },
+
+    async whoami({ commit }) {
+      let user = await fetch("/auth/whoami");
+      try {
+        user = await user.json();
+        console.log(user);
+        commit("setLoggedInUser", user);
+      } catch {
+        console.log("Not authenticated");
+      }
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
