7b8fd5ecb9fff9cd8ba4b06d8a055c1bc4c15e42
André <andreingvarsson@hotmail.com>
2021-01-12 12:09:46 +0100

    working on and rendering out users on admins page to delete or assign new roles.

---
+++ src/components/UserListItem.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <div class="card">
+      <div class="card-body ">
+        <h5 class="card-title">{{user.username}}</h5>
+        <p class="card-text">
+         {{user.email}}
+        </p>
+        <span class="card-text">Role: {{user.roles}}</span>
+      </div> 
+    </div>
+</div>
+</template>
+
+<script>
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component()
+class UserListItem extends Vue {
+      @Prop({
+    required: true,
+  })
+  user;
+    
+}
+
+export default UserListItem
+</script>
+
+<style scoped>
+
+.card-text{
+    font-size: 15px;
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+
+</style>
